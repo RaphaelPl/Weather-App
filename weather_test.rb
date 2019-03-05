a249b2839a0d128a3f16d8bfb1fc66d8bd6c91c5
raphaCA <160218@tuta.io>
2019-03-05 15:01:43 +1000

    second commit with further testing data

--- weather_test.rb
+++ weather_test.rb
@@ -4,3 +4,10 @@ require 'forecast_io'
 ForecastIO.api_key = '05cd0f8759c4e210a946e69d9b339a45'
 
 p spring_hill_forecast = ForecastIO.forecast(-27.4600,153.0260) 
+
+ForecastIO.default_params = { units: 'si' }
+spring_hill_forecast = ForecastIO.forecast(-27.4600,153.0260)
+spring_hill_forecast = spring_hill_forecast.to_hash
+spring_hill_forecast["daily"].each do |key, value|
+   puts "key name: #{key}\tKey type: #{key.class}\tValue type: #{value.class}"
+end
