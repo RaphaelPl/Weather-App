9ce58092790d7e686856d3f323b93b74c9bc3bc9
raphaCA <160218@tuta.io>
2019-03-05 16:18:26 +1000

    committing api changes

--- weather_test.rb
+++ weather_test.rb
@@ -1,7 +1,7 @@
 require 'faraday'
 require 'forecast_io'
 
-ForecastIO.api_key = '05cd0f8759c4e210a946e69d9b339a45'
+ForecastIO.api_key = 'Your API here'
 
 p spring_hill_forecast = ForecastIO.forecast(-27.4600,153.0260) 
 
